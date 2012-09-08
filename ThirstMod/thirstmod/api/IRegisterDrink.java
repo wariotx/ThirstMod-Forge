afbe2a4bc7e8e86b8004b24587c523679022990c
tarun1998 <tsingh.mc@gmail.com>
2012-09-08 17:54:11 +1000

    Added javadoc for IRegisterDrink

--- ThirstMod/thirstmod/api/IRegisterDrink.java
+++ ThirstMod/thirstmod/api/IRegisterDrink.java
@@ -5,5 +5,9 @@ import net.minecraft.src.thirstmod.api.IDrinkAPI;
 
 public interface IRegisterDrink extends IDrinkAPI {
 	
+	/**
+	 * Implement this in your item class and return your item. It will automatically be registered
+	 * next time the game is run. You can then use the onItemDrunk and onItemBeingDrunk events.
+	 */
 	public Item register();
 }
