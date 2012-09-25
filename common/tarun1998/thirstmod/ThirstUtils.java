64217f2676b992de1db946a28033197a9867d22d
Tarun Singh <tarun2011.ts@gmail.com>
2012-09-25 10:59:01 +1000

    version update

--- common/tarun1998/thirstmod/ThirstUtils.java
+++ common/tarun1998/thirstmod/ThirstUtils.java
@@ -13,7 +13,7 @@ import net.minecraft.src.*;
 public class ThirstUtils {
 	public static final String NAME = "Thirst mod";
 	public static final String ID = "ThirstMod";
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.2";
 
 	private static String currentDir;
 
